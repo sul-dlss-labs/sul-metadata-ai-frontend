100755887e629e4df550aab9ae9966a4ceeff92c
Jeremy Nelson <jermnelson@gmail.com>
2025-04-27 17:43:55 -0700

    Working text prompt example

--- src/main.py
+++ src/main.py
@@ -6,26 +6,160 @@ from js import console, FormData
 from pyscript import document, fetch
 
 
+def _clear_fields():
+    console.log("Clearing Fields")
+    instance_json_h2 = document.querySelector("#instance-json-h2")
+    instance_json_h2.classList.add("d-none")
+    card = document.querySelector("#ai-usage-card")
+    card.classList.add("d-none")
+
+    model_name_subtitle = document.querySelector("#model-name")
+    model_name_subtitle.value = ""
+
+    usage_list = document.querySelector("#ai-usage")
+    usage_list.innerHTML = ""
+
+    instance_record = document.querySelector("#instance-json")
+    instance_record.innerHTML = ""
+    folio_instance_json_heading = document.querySelector("#folio-instance-h2")
+    folio_instance_json_heading.classList.add("d-none")
+    folio_instance_json = document.querySelector("#folio-instance-json")
+    folio_instance_json.innerHTML = ""
+
+    folio_status = document.querySelector("#folio-result")
+    folio_status.innerHTML = ""
+
+
+def _generate_parts(parts: list):
+    parts_table = document.createElement("table")
+    for class_ in ["table", "table-sm"]:
+        parts_table.classList.add(class_)
+    parts_header = document.createElement("thead")
+    parts_table.appendChild(parts_header)
+    for class_ in ["bg-dark-subtle", "text-white"]:
+        parts_header.classList.add(class_)
+    parts_header_tr = document.createElement("tr")
+    parts_header.appendChild(parts_header_tr)
+    for header in ["Part Kind", "Time", "Content"]:
+        parts_header_td = document.createElement("td")
+        parts_header_td.innerHTML = header
+        parts_header_tr.appendChild(parts_header_td)
+    parts_tbody = document.createElement("tbody")
+    parts_table.appendChild(parts_tbody)
+    for part in parts:
+        part_tr = document.createElement("tr")
+        parts_tbody.appendChild(part_tr)
+        part_kind_td = document.createElement("td")
+        part_tr.appendChild(part_kind_td)
+        part_kind_value = part.get("part_kind")
+        if "tool_name" in part:
+            part_kind_value = f"{part_kind_value}: <strong>{part['tool_name']}</strong>"
+        part_kind_td.innerHTML = part_kind_value
+        part_time_td = document.createElement("td")
+        part_tr.appendChild(part_time_td)
+        part_time_td.innerHTML = part.get("timestamp", "")
+        part_content_td = document.createElement("td")
+        part_tr.appendChild(part_content_td)
+        part_content_value = part.get("content", "")
+        if "args" in part:
+            part_content_value = f"{part_content_value} {part['args']}"
+        part_content_td.innerHTML = part_content_value
+    return parts_table
+
+
+def _messages(messages: list):
+    ai_messages_table = document.querySelector("#ai-messages-table")
+    ai_messages_tbody = document.querySelector("#ai-messages")
+
+    ai_messages_table.classList.remove("d-none")
+    for message in messages:
+        tr = document.createElement("tr")
+        kind_td = document.createElement("td")
+        kind_td.innerHTML = message["kind"].title()
+        tr.appendChild(kind_td)
+        parts_td = document.createElement("td")
+        parts_td.appendChild(_generate_parts(message.get("parts", [])))
+        tr.appendChild(parts_td)
+        console.log(parts_td, tr)
+        ai_messages_tbody.appendChild(tr)
+
+
+def _model_usage(model_name: str, usage: dict):
+    card = document.querySelector("#ai-usage-card")
+    model_name_subtitle = document.querySelector("#model-name")
+    usage_list = document.querySelector("#ai-usage")
+    usage_stats = usage["usage"]
+    card.classList.remove("d-none")
+    model_name_subtitle.innerHTML = f"<em>Model:</em> {model_name}"
+    request_li = document.createElement("li")
+    request_tokens_li = document.createElement("li")
+    response_tokens_li = document.createElement("li")
+    total_tokens_li = document.createElement("li")
+
+    request_li.innerHTML = (
+        f"<strong>Requests:</strong> {usage_stats.get('requests', -1) }"
+    )
+    request_li.classList.add("list-group-item")
+    usage_list.appendChild(request_li)
+    request_tokens_li.innerHTML = (
+        f"<strong>Request tokens:</strong> {usage_stats.get('request_tokens', -1):,}"
+    )
+    request_tokens_li.classList.add("list-group-item")
+    usage_list.appendChild(request_tokens_li)
+    response_tokens_li.innerHTML = (
+        f"<strong>Response tokens:</strong> {usage_stats.get('response_tokens', -1):,}"
+    )
+    response_tokens_li.classList.add("list-group-item")
+    usage_list.appendChild(response_tokens_li)
+    total_tokens_li.innerHTML = (
+        f"<strong>Total tokens:</strong> {usage_stats.get('total_tokens', -1):,}"
+    )
+    total_tokens_li.classList.add("list-group-item")
+    usage_list.appendChild(total_tokens_li)
+
+
 def display_instance_result(result):
+
     instance_record = document.querySelector("#instance-json")
     instance_record_heading = document.querySelector("#instance-json-h2")
     instance_record.innerHTML = json.dumps(result.get("record"), indent=2)
     instance_record_heading.classList.remove("d-none")
-    if result["folio_response"] is None:
-        folio_status.innerHTML = (
-            """<h2 class="text-success align-middle">Success!</h2>"""
+    folio_status = document.querySelector("#folio-result")
+
+    if "error" in result:
+        folio_status.innerHTML = f"""<div class="alert alert-danger" role="alert">
+<h2 class="alert-heading">ERROR!</h2><p>{result['error']}</p>
+</div>"""
+    else:
+        folio_url_elem = document.querySelector("#folio-url")
+        json_recs = document.querySelector("#json-records")
+        json_recs.classList.remove("d-none")
+        folio_instance_json_heading = document.querySelector("#folio-instance-h2")
+        folio_instance_json = document.querySelector("#folio-instance-json")
+        folio_instance_json.innerHTML = json.dumps(
+            result.get("folio_response"), indent=2
         )
-    elif "error" in result["folio_response"]:
-        folio_status.innerHTML = (
-            f"<h3>ERROR!</h3><p>{{ result['folio_response']['error'] }}</p>"
+
+        instance_url = (
+            f"{folio_url_elem.value}/inventory/view/{result['folio_response']['id']}"
         )
+        folio_status.innerHTML = f"""<h2 class="text-success align-middle">Success!</h2>
+            <a href="{instance_url}">{instance_url}</a>"""
+        if result["usage"] is None and result["ai_messages"] is None:
+            console.log(f"Usage and AI Messages missing from response")
+            return
+        _model_usage(result["model_name"], result["usage"])
+        _messages(result["usage"]["messages"])
 
 
 async def submit_prompt(event):
     """Submits textual prompt to edge-ai to generate Inventory Instance"""
+    loading_spinner = document.querySelector("#load-spinner")
+    loading_spinner.classList.remove("d-none")
+    _clear_fields()
     text_prompt = document.querySelector("#text-prompt")
     edge_ai = document.querySelector("#edge-ai-url")
-    folio_status = document.querySelector("#folio-result")
+
     payload = {
         "method": "POST",
         "headers": {"Content-Type": "application/json"},
@@ -34,11 +168,13 @@ async def submit_prompt(event):
     response = await fetch(f"{edge_ai.value}/inventory/instance/generate", **payload)
     result = await response.json()
     display_instance_result(result)
+    loading_spinner.classList.add("d-none")
     text_prompt.innerHTML = ""
 
 
 async def upload_image(event):
     """Submits book cover image to edge_ai to generate Inventory Instance"""
+    _clear_fields()
     upload_image = document.querySelector("#cover-image")
 
     edge_ai = document.querySelector("#edge-ai-url")
